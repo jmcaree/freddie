06c36a9577c496e9bb2b036ad71316d41046bc6f
John McAree <john.mcaree@gmail.com>
2020-10-26 20:38:15 +0000

    messageResponder tests

--- src/services/__tests__/messageResponder.spec.ts
+++ src/services/__tests__/messageResponder.spec.ts
@@ -1,16 +1,46 @@
-import { Message } from "discord.js";
+import { Client, Guild, Message, TextChannel } from "discord.js";
 import MessageResponder from "../messageResponder";
 import PingFinder from "../pingFinder";
 
-jest.mock("../pingFinder");
+const mockIsPing = jest.fn();
+jest.mock("../pingFinder", () => {
+    return jest.fn().mockImplementation(() => {
+        return {
+            isPing: mockIsPing,
+        };
+    });
+});
 jest.mock("discord.js");
 
 describe("MessageResponder", () => {
     const mockPingFinder = new PingFinder();
-    const mockMessage = Message as jest.Mock;
+    let mockClient: Client;
+    let mockGuild: Guild;
+    let mockMessage: Message;
+    let mockTextChannel: TextChannel;
     const messageResponder = new MessageResponder(mockPingFinder);
 
-    it.skip("should reply", async () => {
+    beforeEach(() => {
+        mockIsPing.mockClear();
+        mockClient = new Client();
+        mockGuild = new Guild(mockClient, {});
+        mockTextChannel = new TextChannel(mockGuild);
+        mockMessage = new Message(mockClient, {}, mockTextChannel);
+    });
+
+    it("should reply", async () => {
+        mockIsPing.mockReturnValueOnce(true);
+
         await messageResponder.handle(mockMessage);
+
+        expect(mockMessage.reply).toHaveBeenCalled();
+    });
+
+    it("should throw when ping not found", async () => {
+        mockIsPing.mockReturnValueOnce(false);
+
+        await messageResponder.handle(mockMessage).catch(() => {
+            expect(mockMessage.reply).not.toHaveBeenCalled();
+        });
     });
 });
